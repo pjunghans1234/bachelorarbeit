d3eddbe88d44b2bd3c9952da7ab4d9554d85fa30
Philipp Janik JUNGHANS <pjunghans@student.ethz.ch>
2026-08-01 21:21:56 +0200

    mesmer und modul start

---
+++ model/__init__.py
@@ -0,0 +1,15 @@
+#Pib
+"""
+blup
+"""
+
+from importlib.metadata import version as _get_version
+
+from model import (
+    stats
+)
+# "new" "modules"
+__all__ = [
+    "stats",
+]
+
---
+++ model/stats/__init__.py
@@ -0,0 +1,8 @@
+from model.stats._parallel_linear_regression import ParLinearRegression
+from model.stats._linear_regression import LinearRegression
+
+__all__ = [
+    # linear regression
+    "LinearRegression",
+    "ParLinearRegression"
+    ]
